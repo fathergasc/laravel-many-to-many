b1f6c14af517416ad2cc81089d435a99b7e1400b
fathergasc <quaglioniandrea@gmail.com>
2022-10-12 14:54:55 +0200

    add: tag model, tag seeder, tags table migration

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Tag;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['Calciomercato', 'Serie A', 'Legabasket', 'NBA', 'Ferrari'];
+
+        foreach ($tags as $tag) {
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::slug($tag);
+            $newTag->save();
+        }
+    }
+}
