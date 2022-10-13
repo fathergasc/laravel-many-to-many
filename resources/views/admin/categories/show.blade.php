088df6cb006c3fdbcd5ee08f1331bc96b0bf204c
fathergasc <quaglioniandrea@gmail.com>
2022-10-13 17:25:43 +0200

    add: display posts related to category + gotopost btn in categories.show

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -7,6 +7,29 @@
     <h3>Slug: {{$category->slug}}</h3>
 
 
+    <h4>Related posts:</h4>
+    @if (count($category->posts))
+    <div>
+    <table class="table table-dark table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($category->posts as $post)
+            <th scope="row">{{$post->id}}</th>
+            <td>{{$post->title}}</td>
+            <td>{{$post->slug}}</td>
+            <td><a class="btn btn-warning m-1" href="{{route('admin.posts.show', ['post' => $post->id])}}">Go to post</a></td>
+            @endforeach
+        </tbody>
+    </table>
+    @endif
+
     <a class="btn btn-primary" href="{{route('admin.categories.index')}}">Back to categories list</a>
 </div>
 
